23c8f6ac3e2964ed6cd75428e079282aeec20a9f
Badagala Jatin <badagalajatin@gmail.com>
2026-08-28 14:47:45 +0530

    Implement mixins for multiple inheritance simulation
    Refactor multiple inheritance example to use mixins for Child class.

--- assignments/Inheritance_types/multiple_inheritance.js
+++ assignments/Inheritance_types/multiple_inheritance.js
@@ -1,21 +1,25 @@
 //JavaScript does not support multiple inheritance directly using classes, but we can achieve similar behavior using mixins.
 class Father {
-  fatherProperty() {
-    console.log("Father's property");
-  }
+    fatherProperty() {
+        console.log("Father's property");
+    }
 }
 
 class Mother {
-  motherProperty() {
-    console.log("Mother's property");
-  }
+    motherProperty() {
+        console.log("Mother's property");
+    }
 }
 
-class Child extends Father {
-  motherProperty() {
-    console.log("Mother's property");
-  }
+class Child {
 }
+
+// Adding methods from both classes
+Object.assign(
+    Child.prototype,
+    Father.prototype,
+    Mother.prototype
+);
 
 let child = new Child();
 
